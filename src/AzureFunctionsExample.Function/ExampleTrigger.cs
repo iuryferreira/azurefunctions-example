1d9d5c9915678a6315d6879550a929bea8f64f83
Iury Ferreira <iury.ferreira@vaivoa.com>
2021-09-20 15:03:14 -0300

    🎉 add initial project

---
+++ src/AzureFunctionsExample.Function/ExampleTrigger.cs
@@ -0,0 +1,25 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsExample.Function
+{
+    public static class ExampleTrigger
+    {
+        [Function("ExampleTrigger")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("ExampleTrigger");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
