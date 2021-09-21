6e898ba66d85a766ce46784df22d496b6550f330
Iury Ferreira <iury.ferreira@vaivoa.com>
2021-09-21 17:03:45 -0300

    teste

--- src/AzureFunctionsExample.Function/ExampleTrigger.cs
+++ src/AzureFunctionsExample.Function/ExampleTrigger.cs
@@ -1,14 +1,18 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
 namespace AzureFunctionsExample.Function
 {
     public static class ExampleTrigger
     {
+
         [Function("ExampleTrigger")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [CosmosDB(databaseName: "my-database", collectionName: "my-container", ConnectionStringSetting = "CosmosDB")] IAsyncCollector<dynamic> documents,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("ExampleTrigger");
